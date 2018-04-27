8e896f8c1683ff63052bd79e40dbcbf0d925ee95
SrilakshmiMaddali <35358046+SrilakshmiMaddali@users.noreply.github.com>
2018-04-26 23:04:24 -0500

    Image loading improvements
    - Add progress bar
    - Add callback to image loader, to handle error/success response from image loading library.

--- sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,11 +3,13 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.ContentLoadingProgressBar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
@@ -17,6 +19,7 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
     protected static final String NA = "Not Available";
+    ContentLoadingProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mProgressBar = findViewById(R.id.loadingprogress);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -49,7 +53,17 @@ public class DetailActivity extends AppCompatActivity {
         populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .into(ingredientsIv, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        mProgressBar.hide();
+                    }
+
+                    @Override
+                    public void onError() {
+                        Toast.makeText(DetailActivity.this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
+                    }
+                });
 
         setTitle(sandwich.getMainName());
     }
@@ -60,6 +74,7 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI(@NonNull Sandwich sandwich) {
+        mProgressBar.show();
         TextView placeOfOrigin = (TextView)findViewById(R.id.origin_tv);
         String place = sandwich.getPlaceOfOrigin();
         if (!place.isEmpty()) {
